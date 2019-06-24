2d38fabe0c9d35b2a482ac84098e67762b5c77d4
Silavsale <silavsale@gmail.com>
2019-06-24 17:58:20 -0400

    add bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    const API_TEAMS = "https://statsapi.web.nhl.com/api/v1/teams";
+
+
+
+
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link, NavLink, withRouter} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+      <div>
+          <h1>Hello</h1>
+      </div>
+    )
+
+};
+
+export default Navbar;
